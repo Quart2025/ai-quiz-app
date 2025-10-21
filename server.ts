51327120a969b9c58288f078ad77f26ac2b8ffbf
Court Pitts <pittscourt@Courts-MacBook-Pro.local>
2025-10-21 16:39:38 -0600

    added comments for clearer interpretation of code

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
-// Middleware to parse incoming JSON requests
+// Middleware to parse incoming JSON request
 app.use(express.json());
 
 // Serve static files (index.html, etc.) from current directory
